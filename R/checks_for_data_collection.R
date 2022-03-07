147b53b42721cc2ed8822dbce25783c0e94a539e
Anthony Twesigye <twesigye10@gmail.com>
2022-03-07 16:47:45 +0300

    removing point numbers

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -13,11 +13,11 @@ df_tool_data <- readxl::read_excel("inputs/BNA_data.xlsx") %>%
          i.check.start_date = as_date(start),
          i.check.enumerator_id = enumerator_id,
          i.check.district_name = district_name,
-         i.check.point_number = point_number,
+         # i.check.point_number = point_number,
          start = as_datetime(start),
          end = as_datetime(end)) %>% 
   filter(consent == "yes", age >= 18, i.check.start_date > as_date("2022-03-20"), 
-         !str_detect(string = point_number, pattern = fixed('test', ignore_case = TRUE))
+         # !str_detect(string = point_number, pattern = fixed('test', ignore_case = TRUE))
   )
 
 df_survey <- readxl::read_excel("inputs/BNA_quant_tool.xlsx", sheet = "survey")
