e71ddbe4a478b9b15073f0aa47663632c0abde36
Anthony Twesigye <twesigye10@gmail.com>
2022-03-07 16:13:31 +0300

    check duplicate uuid

--- R/support_functions.R
+++ R/support_functions.R
@@ -103,3 +103,29 @@ extract_other_data <- function(input_tool_data, input_survey, input_choices) {
            uuid_cl,
            so_sm_choices)
 }
+
+# check duplicate uuid ----------------------------------------------------------
+
+check_duplicates_by_uuid <- function(input_tool_data) {
+  input_tool_data %>% 
+    group_by(i.check.uuid) %>% 
+    mutate(rank = row_number()) %>% 
+    filter(rank > 1) %>%  
+    mutate(
+      i.check.type = "remove_survey",
+      i.check.name = "point_number",
+      i.check.current_value = "",
+      i.check.value = "",
+      i.check.issue_id = "duplicate_uuid",
+      i.check.issue = "The uuid: {i.check.uuid} is duplicate in the data",
+      i.check.other_text = "",
+      i.check.checked_by = "",
+      i.check.checked_date = as_date(today()),
+      i.check.comment = "", 
+      i.check.reviewed = "",
+      i.check.adjust_log = "",
+      i.check.uuid_cl = "",
+      i.check.so_sm_choices = "")%>% 
+    dplyr::select(starts_with("i.check"))%>% 
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
