52198fe4561970b057e67d8789f24ab214e98093
Anthony Twesigye <twesigye10@gmail.com>
2022-03-07 16:42:53 +0300

    read data and tools

---
+++ R/checks_for_data_collection.R
@@ -0,0 +1,24 @@
+# checks for data collection
+
+library(tidyverse)
+library(lubridate)
+library(glue)
+
+source("R/support_functions.R")
+
+# read data ---------------------------------------------------------------
+
+df_tool_data <- readxl::read_excel("inputs/BNA_data.xlsx") %>% 
+  mutate(i.check.uuid = `_uuid`,
+         i.check.start_date = as_date(start),
+         i.check.enumerator_id = enumerator_id,
+         i.check.district_name = district_name,
+         i.check.point_number = point_number,
+         start = as_datetime(start),
+         end = as_datetime(end)) %>% 
+  filter(consent_two == "yes", respondent_age >= 12, i.check.start_date > as_date("2022-01-30"), 
+         !str_detect(string = point_number, pattern = fixed('test', ignore_case = TRUE))
+  )
+
+df_survey <- readxl::read_excel("inputs/BNA_quant_tool.xlsx", sheet = "survey")
+df_choices <- readxl::read_excel("inputs/BNA_quant_tool.xlsx", sheet = "choices")
