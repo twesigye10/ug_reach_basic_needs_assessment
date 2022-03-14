ab7d871183153a9dba2ac806c02ef370e0233ddf
Anthony Twesigye <twesigye10@gmail.com>
2022-03-14 16:28:00 +0300

    df_disagree_child_marriage_but_agree_girl_to_be_married

--- R/logical_checks_for_data_collection.R
+++ R/logical_checks_for_data_collection.R
@@ -58,3 +58,57 @@ if(exists("df_agree_child_marriage_but_disagree_circumstances")){
   }
 }
 
+# okay_arrange_marriage_3
+df_agree_child_marriage_but_disagree_girl_to_be_married <- df_tool_data %>% 
+  filter(okay_arrange_marriage %in% c("agree", "strongly_agree") &
+           okay_girl_to_be_married %in% c("disagree", "strongly_disagree", "neither_agree_nor_agree")) %>% 
+  mutate(i.check.type = "change_response",
+         i.check.name = "okay_arrange_marriage",
+         i.check.current_value = okay_arrange_marriage,
+         i.check.value = "",
+         i.check.issue_id = "okay_arrange_marriage_3",
+         i.check.issue = glue("okay_girl_to_be_married: {okay_girl_to_be_married}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "accept", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.uuid_cl = "",
+         i.check.so_sm_choices = "") %>% 
+  dplyr::select(starts_with("i.check")) %>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+if(exists("df_agree_child_marriage_but_disagree_girl_to_be_married")){
+  if(nrow(df_agree_child_marriage_but_disagree_girl_to_be_married) > 0){
+    logic_seperate_output$df_agree_child_marriage_but_disagree_girl_to_be_married <- df_agree_child_marriage_but_disagree_girl_to_be_married
+  }
+}
+
+# okay_arrange_marriage_4
+df_disagree_child_marriage_but_agree_girl_to_be_married <- df_tool_data %>% 
+  filter(okay_arrange_marriage %in% c("disagree", "strongly_disagree", " neiter_agree_not_agree") &
+           okay_girl_to_be_married %in% c("agree", "strongly_agree")) %>% 
+  mutate(i.check.type = "change_response",
+         i.check.name = "okay_arrange_marriage",
+         i.check.current_value = okay_arrange_marriage,
+         i.check.value = "",
+         i.check.issue_id = "okay_arrange_marriage_4",
+         i.check.issue = glue("okay_girl_to_be_married: {okay_girl_to_be_married}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "accept", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.uuid_cl = "",
+         i.check.so_sm_choices = "") %>% 
+  dplyr::select(starts_with("i.check")) %>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+if(exists("df_disagree_child_marriage_but_agree_girl_to_be_married")){
+  if(nrow(df_disagree_child_marriage_but_agree_girl_to_be_married) > 0){
+    logic_seperate_output$df_disagree_child_marriage_but_agree_girl_to_be_married <- df_disagree_child_marriage_but_agree_girl_to_be_married
+  }
+}
+
