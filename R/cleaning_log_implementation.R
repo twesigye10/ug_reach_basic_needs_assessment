ac161babe1dd980aefa7d5404abb6f0498161935
Anthony Twesigye <twesigye10@gmail.com>
2022-04-12 16:00:24 +0300

    cleaning: hh_roster

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -67,3 +67,15 @@ df_cleaned_data <- implement_cleaning_support(input_df_raw_data = df_raw_data,
                                               input_df_cleaning_log = df_cleaning_log %>% filter(name %in% colnames(df_raw_data)))
 
 write_csv(df_cleaned_data, file = paste0("outputs/", butteR::date_file_prefix(), "_clean_data_bna.csv"))
+
+# hh_roster
+df_cleaning_log_hh_roster <- df_cleaning_log %>% 
+  filter(uuid %in% df_raw_data_hh_roster$`_uuid`, name %in% colnames(df_raw_data_hh_roster))
+
+df_cleaned_hh_roster_data <- implement_cleaning_support(input_df_raw_data = df_raw_data_hh_roster, 
+                                                                                    input_df_survey = df_survey, 
+                                                                                    input_df_choices = df_choices, 
+                                                                                    input_df_cleaning_log = df_cleaning_log_hh_roster) %>% 
+  select(any_of(colnames(hh_roster)))
+
+write_csv(df_cleaned_hh_roster_data, file = paste0("outputs/", butteR::date_file_prefix(), "_clean_hh_roster_data_bna.csv"))
